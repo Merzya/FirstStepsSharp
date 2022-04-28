80922dd4f22a79bd11ac11a9554961b6c91d7302
Merzya <morgershterm@gmail.com>
2022-04-29 00:00:12 +0300

    array

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace consolesapp
+{
+    class program
+    {
+    
+
+        static void Main(string[] args)
+        {
+            ReturnComAge(args);
+        }
+
+        private static void ReturnComAge(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                int Age = GetIntFromArgs(args[0]);
+
+                string[] status = new string[]
+                {"not born",
+                 "small",
+                 "old",
+                };
+
+                if (Age >= 18)
+                {
+                    Console.WriteLine(status[2]);
+                }
+                else if (Age is > 0 and < 18)
+                {
+                    Console.WriteLine(status[1]);
+                }
+                else
+                {
+                    Console.WriteLine(status[0]);
+                }
+            }
+           
+        }
+
+        private static int GetIntFromArgs(string arg)
+        {
+            if (int.TryParse(arg, out int value))
+            {
+                return value;
+            }
+            else { return 0; }
+        }
+    }
+}
+
