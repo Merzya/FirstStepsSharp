e2c548d67fd53dc269deeb096abbdc29a15787b9
Merzya <104583126+Merzya@users.noreply.github.com>
2022-04-28 20:20:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace consolesapp
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            decimal number = 123;
+
+            float numWithDot = 123.321F;
+            double numWithoutDot = 321.123;
+          
+            Console.WriteLine( numWithoutDot + numWithDot);
+            Console.WriteLine(number);
+        }
+    }
+}
+
